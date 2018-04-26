d9d638de47996f0ca3483f8097b27cdfb75626f2
Gilles Bouthenot <misc@atomas.com>
2018-04-26 09:52:32 +0200

    add progress

--- desc2.js
+++ desc2.js
@@ -62,7 +62,7 @@ function headers () {
   .year-stats { display:inline; }
 
   .show { margin-bottom:1em; display:table; }
-  .show-scr { display:table-cell; width:160px; height:90px; }
+  .show-scr { width:160px; height:90px; margin-bottom:2px; }
   .show-scr img { width:inherit; height:90px; }
   .show-desc { display:table-cell; vertical-align:top; padding-left:.5em; }
   .show-name { display:block; font-size:150%; margin-right:1em; }
@@ -177,6 +177,108 @@ function headers () {
   .mos97 .mos { background-position:77.77% 99.99%; }
   .mos98 .mos { background-position:88.88% 99.99%; }
   .mos99 .mos { background-position:99.99% 99.99%; }
+
+  .show-mos .prog { height:2px; background-color:#f00; }
+  .mos0  .prog { width:0%; }
+  .mos1  .prog { width:1%; }
+  .mos2  .prog { width:2%; }
+  .mos3  .prog { width:3%; }
+  .mos4  .prog { width:4%; }
+  .mos5  .prog { width:5%; }
+  .mos6  .prog { width:6%; }
+  .mos7  .prog { width:7%; }
+  .mos8  .prog { width:8%; }
+  .mos9  .prog { width:9%; }
+  .mos10 .prog { width:10%; }
+  .mos11 .prog { width:11%; }
+  .mos12 .prog { width:12%; }
+  .mos13 .prog { width:13%; }
+  .mos14 .prog { width:14%; }
+  .mos15 .prog { width:15%; }
+  .mos16 .prog { width:16%; }
+  .mos17 .prog { width:17%; }
+  .mos18 .prog { width:18%; }
+  .mos19 .prog { width:19%; }
+  .mos20 .prog { width:20%; }
+  .mos21 .prog { width:21%; }
+  .mos22 .prog { width:22%; }
+  .mos23 .prog { width:23%; }
+  .mos24 .prog { width:24%; }
+  .mos25 .prog { width:25%; }
+  .mos26 .prog { width:26%; }
+  .mos27 .prog { width:27%; }
+  .mos28 .prog { width:28%; }
+  .mos29 .prog { width:29%; }
+  .mos30 .prog { width:30%; }
+  .mos31 .prog { width:31%; }
+  .mos32 .prog { width:32%; }
+  .mos33 .prog { width:33%; }
+  .mos34 .prog { width:34%; }
+  .mos35 .prog { width:35%; }
+  .mos36 .prog { width:36%; }
+  .mos37 .prog { width:37%; }
+  .mos38 .prog { width:38%; }
+  .mos39 .prog { width:39%; }
+  .mos40 .prog { width:40%; }
+  .mos41 .prog { width:41%; }
+  .mos42 .prog { width:42%; }
+  .mos43 .prog { width:43%; }
+  .mos44 .prog { width:44%; }
+  .mos45 .prog { width:45%; }
+  .mos46 .prog { width:46%; }
+  .mos47 .prog { width:47%; }
+  .mos48 .prog { width:48%; }
+  .mos49 .prog { width:49%; }
+  .mos50 .prog { width:50%; }
+  .mos51 .prog { width:51%; }
+  .mos52 .prog { width:52%; }
+  .mos53 .prog { width:53%; }
+  .mos54 .prog { width:54%; }
+  .mos55 .prog { width:55%; }
+  .mos56 .prog { width:56%; }
+  .mos57 .prog { width:57%; }
+  .mos58 .prog { width:58%; }
+  .mos59 .prog { width:59%; }
+  .mos60 .prog { width:60%; }
+  .mos61 .prog { width:61%; }
+  .mos62 .prog { width:62%; }
+  .mos63 .prog { width:63%; }
+  .mos64 .prog { width:64%; }
+  .mos65 .prog { width:65%; }
+  .mos66 .prog { width:66%; }
+  .mos67 .prog { width:67%; }
+  .mos68 .prog { width:68%; }
+  .mos69 .prog { width:69%; }
+  .mos70 .prog { width:70%; }
+  .mos71 .prog { width:71%; }
+  .mos72 .prog { width:72%; }
+  .mos73 .prog { width:73%; }
+  .mos74 .prog { width:74%; }
+  .mos75 .prog { width:75%; }
+  .mos76 .prog { width:76%; }
+  .mos77 .prog { width:77%; }
+  .mos78 .prog { width:78%; }
+  .mos79 .prog { width:79%; }
+  .mos80 .prog { width:80%; }
+  .mos81 .prog { width:81%; }
+  .mos82 .prog { width:82%; }
+  .mos83 .prog { width:83%; }
+  .mos84 .prog { width:84%; }
+  .mos85 .prog { width:85%; }
+  .mos86 .prog { width:86%; }
+  .mos87 .prog { width:87%; }
+  .mos88 .prog { width:88%; }
+  .mos89 .prog { width:89%; }
+  .mos90 .prog { width:90%; }
+  .mos91 .prog { width:91%; }
+  .mos92 .prog { width:92%; }
+  .mos93 .prog { width:93%; }
+  .mos94 .prog { width:94%; }
+  .mos95 .prog { width:95%; }
+  .mos96 .prog { width:96%; }
+  .mos97 .prog { width:97%; }
+  .mos98 .prog { width:98%; }
+  .mos99 .prog { width:99%; }
   </style>
   <script src='../../../../../shows.js' type='text/javascript'></script>
   `
@@ -318,6 +420,7 @@ function createPartnerFamilyYearShows (dir, part, fam, year) {
     out += `<div class='show' data-id='${show.id_show}'>`
     out += `  <div class='show-mos' style='display:none;'>`
     out += `    <div class='mos' style='background-image:url("${mos}");'></div>`
+    out += `    <div class='prog'></div>`
     out += `  </div>`
     out += `  <div class='show-scr'><img src='${scr}' /></div>`
     out += `  <div class='show-desc'>`
